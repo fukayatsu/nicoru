aa1237dbbf26ec540cefc01bd3f449597fe0ed61
fukayatsu <fukayatsu@gmail.com>
2013-09-30 23:42:44 +0900

    Add view

--- Nicoru/AppDelegate.rb
+++ Nicoru/AppDelegate.rb
@@ -8,8 +8,17 @@
 
 class AppDelegate
     attr_accessor :window
+    attr_accessor :board
+    attr_accessor :keyword
+    attr_accessor :main_screen
     def applicationDidFinishLaunching(a_notification)
         # Insert code here to initialize your application
+        window.opaque = false
+        window.backgroundColor =  NSColor.colorWithCalibratedWhite(1.0, alpha:0.0)
+    end
+    
+    def start(sender)
+        # board.stringValue = 'start'
     end
 end
 
