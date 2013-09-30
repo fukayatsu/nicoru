0ad40a5810b889650b86d64bbf2ad8383a30cdd2
fukayatsu <fukayatsu@gmail.com>
2013-10-01 00:23:24 +0900

    [wip]コメントっぽいものを表示

--- Nicoru/AppDelegate.rb
+++ Nicoru/AppDelegate.rb
@@ -11,6 +11,7 @@ class AppDelegate
     attr_accessor :board
     attr_accessor :keyword
     attr_accessor :main_screen
+    
     def applicationDidFinishLaunching(a_notification)
         # Insert code here to initialize your application
         window.opaque = false
@@ -19,6 +20,19 @@ class AppDelegate
     
     def start(sender)
         # board.stringValue = 'start'
+        
+        main_screen.addSubview text(100, 200, 15, "ほげほげー")
+    end
+    
+    def text(x, y, size, body)
+        textField = NSTextField.alloc.initWithFrame NSMakeRect(x,y,0,0)
+        textField.stringValue = body
+        textField.editable = false
+        textField.drawsBackground = false
+        textField.bezeled = false
+        textField.selectable = false
+        textField.font = NSFont.fontWithName("HiraKakuPro-W3", size:size)
+        textField.sizeToFit
     end
 end
 
