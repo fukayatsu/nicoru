dfacb7057bf699977f1b827efa1f0ffbcc38ea5e
fukayatsu <fukayatsu@gmail.com>
2013-10-01 01:22:44 +0900

    影付き文字の描写テスト

--- Nicoru/AppDelegate.rb
+++ Nicoru/AppDelegate.rb
@@ -11,27 +11,44 @@ class AppDelegate
     attr_accessor :board
     attr_accessor :keyword
     attr_accessor :main_screen
-    
+
     def applicationDidFinishLaunching(a_notification)
         # Insert code here to initialize your application
         window.opaque = false
         window.backgroundColor =  NSColor.colorWithCalibratedWhite(1.0, alpha:0.0)
     end
-    
+
     def start(sender)
         # board.stringValue = 'start'
-        
-        main_screen.addSubview text(100, 200, 15, "ほげほげー")
+
+        main_screen.addSubview text(100, 200, 30, "ほげほげー")
     end
-    
+
     def text(x, y, size, body)
-        textField = NSTextField.alloc.initWithFrame NSMakeRect(x,y,0,0)
-        textField.stringValue = body
+        textField = NSTextField.alloc.initWithFrame NSMakeRect(x, y, 0, 0)
+
+        str = NSMutableAttributedString.alloc.initWithString(body)
+
+        str.addAttribute(NSForegroundColorAttributeName,
+                         value: NSColor.whiteColor,
+                         range: NSMakeRange(0, body.length))
+
+         shadow = NSShadow.alloc.init
+         shadow.shadowOffset = [1.0, 1.0]
+         shadow.shadowColor = NSColor.blackColor
+         shadow.shadowBlurRadius = 5.0
+
+         str.addAttribute(NSShadowAttributeName,
+                          value: shadow,
+                          range: NSMakeRange(0, body.length))
+
+
+        textField.attributedStringValue = str
         textField.editable = false
         textField.drawsBackground = false
         textField.bezeled = false
         textField.selectable = false
-        textField.font = NSFont.fontWithName("HiraKakuPro-W3", size:size)
+        textField.font = NSFont.fontWithName("HiraKakuPro-W6", size:size)
         textField.sizeToFit
     end
 end
